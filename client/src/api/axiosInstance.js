d9e763b0208e152d416111397c090b7e1cb7750a
Ankit Kumar <ankit8809@outlook.com>
2026-09-19 13:32:52 +0530

    Use VITE_API_URL for API Calls

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL || "httlp://localhost:5000";
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: API_URL,
 });
 
 // Attach the JWT to every request automatically
