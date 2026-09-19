6fa55232d423b8baf48cad623c93bb372beae9c0
Ankit Kumar <ankit8809@outlook.com>
2026-09-19 13:16:25 +0530

    Use CLIENT_URL for CORS

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
